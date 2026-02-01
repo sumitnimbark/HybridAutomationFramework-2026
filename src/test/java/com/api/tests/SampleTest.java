59a50840bf46658711b9d1ad1403362ae2d70b1b
Sumit K Nimbark <Sumit K Nimbark@LAPTOP-80OJH620>
2026-02-01 21:36:13 +0530

    1. Added first RestAssured Sample test 2. Fixed Rest Assured latest version compatibility issue with Java 11

---
+++ src/test/java/com/api/tests/SampleTest.java
@@ -0,0 +1,26 @@
+package com.api.tests;
+
+import io.restassured.RestAssured;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+
+		RestAssured.baseURI = "https://petstore.swagger.io/";
+		
+		RestAssured
+			.given()
+			.header("accept", "application/json")
+			.and()
+			.pathParams("petId", "1")
+			.log().all()
+			
+			.when()
+			.get("v2/pet/{petId}")
+			
+			.then()
+			.assertThat().statusCode(200)
+			.log().all();
+	}
+
+}
