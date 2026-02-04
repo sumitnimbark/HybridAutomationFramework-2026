ed674ece2c0f38c7d423e1152f0d578c25eaa08d
sumitnimbark <it2003.sumit@gmail.com>
2026-02-04 21:38:29 +0530

    Added TestNG and updated test cases

--- src/test/java/com/api/tests/AddOrder.java
+++ src/test/java/com/api/tests/AddOrder.java
@@ -1,26 +1,33 @@
 package com.api.tests;
 
-import com.google.gson.Gson;
 import com.pojo.AddOrderPOJO;
+
+import static com.utility.TestUtility.*;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 
 public class AddOrder {
-
-	public static void main(String[] args) {
-		
+	
+	AddOrderPOJO order1;
+	
+	@BeforeMethod
+	public void setup()
+	{
 		RestAssured.baseURI = "https://petstore.swagger.io/";
-		
-		AddOrderPOJO order1 = new AddOrderPOJO(1, 1, 2, "2024-06-10T14:12:10.123Z", true);
-		
-		Gson gson = new Gson();
-		String orderJson = gson.toJson(order1);
+		order1 = new AddOrderPOJO(1, 1, 2, "2024-06-10T14:12:10.123Z", true);
+	}
+
+	@Test(description = "Add Order to the Store", groups = {"API"})
+	public void addOrder() {
 		
 		RestAssured
 			.given()
 				.header("Content-Type", "application/json")
-				.and()
-				.body(orderJson)
+			.and()
+				.body(convertPOJOToJson(order1))
 				.log().all()
 			
 			.when()
--- src/test/java/com/utility/TestUtility.java
+++ src/test/java/com/utility/TestUtility.java
@@ -1,5 +1,15 @@
 package com.utility;
 
+import com.google.gson.Gson;
+
 public class TestUtility {
+	
+	public static String convertPOJOToJson(Object object) {
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(object);
+		return json;
+		
+	}
 
 }
